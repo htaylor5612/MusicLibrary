9e79bc7eb55eb80e15ca753c10aab439090fddde
ahsvanstrom <amelia.svanstrom@students.mq.edu.au>
2024-10-27 16:31:26 +1100

    Update Song.java

--- Song.java
+++ Song.java
@@ -1,1 +1,24 @@
+import java.io.*;
+import java.util.*;
 
+public class Song {
+    String name;
+    String artist;
+    String album;
+    int duration;
+
+
+public Song (String name, String artist, String album, int duration){
+this.name = name;
+this.artist = artist;
+this.album = album;
+this.duration = duration;
+}
+
+public String toString() {
+    return "Title: " + name + "\n" +
+           "Artist: " + artist + "\n" +
+           "Album: " + album + "\n" +
+           "Duration: " + duration + " seconds\n";
+}
+}
