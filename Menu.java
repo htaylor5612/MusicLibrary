5b3c1500573b429d12d3f0c0fd9f303a151be335
ahsvanstrom <amelia.svanstrom@students.mq.edu.au>
2024-10-27 16:34:15 +1100

    Create Menu.java

---
+++ Menu.java
@@ -0,0 +1,14 @@
+public class Menu {
+        public void displayMenu() {
+            System.out.println("\nChoose an option:");
+            System.out.println("1. Play a specific song");
+            System.out.println("2. Shuffle play");
+            System.out.println("3. Add a song");
+            System.out.println("4. Remove a song");
+            System.out.println("5. Display playlist");
+            System.out.println("6. Skip to next song");
+            System.out.println("7. Go to previous song");
+            System.out.println("8. Stop Listening");
+        }
+    }
+
