9a79c3601f245d66dde0d4e114d86d0193e48cdd
ahsvanstrom <amelia.svanstrom@students.mq.edu.au>
2024-10-27 16:34:42 +1100

    Create Musiclibrary.java

---
+++ Musiclibrary.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Musiclibrary {
+    ArrayList <Song> songs;
+
+    public void addSong(Song song){
+        songs.add(song);
+        }
+       
+ public List<Song> getSongs() {
+            return songs; // Return the list of Song objects
+        }
+
+
+}
+
+      
+        
+
+
+ 
+ 
